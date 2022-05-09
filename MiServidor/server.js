cc1f20ca8be1667fd1c8cf7e0dba1ff7a496f188
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-05-08 19:03:50 -0500

    Feat: Added server basic

--- MiServidor/server.js
+++ MiServidor/server.js
@@ -1,12 +1,26 @@
 const http = require('http');
 
 const PORT = 5000;
+const books = [
+    { "title": "Libro de programación", "autor": "Sebastian Carrillo"},
+    { "title": "1984", "autor": "George Orwell"},
+    { "title": "Harry Potter", "autor": "Heli Ojeda"}
+];
 
 const server = http.createServer((req, res) => {
     // Lo que recibe el servidor
-    console.log(req);
+    // console.log(req);
+    // const { headers, url, method } = req;
+    // console.log(headers, url, method);
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'application/json')
+    // res.write('<h1>Esto es una prueba</h1>');
+    
     // Enviar una respuesta al cliente
-    res.end();
+    res.end(
+        JSON.stringify({data: books})
+    );
 
 });
 
@@ -15,7 +29,7 @@ server.listen(PORT, () => {
     console.log('Server listen in ', PORT)
 });
 
-
+// !Video 004
 
 
 
